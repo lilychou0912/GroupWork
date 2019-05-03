b9cf12c92531bd7fe9c2d6b3e7b3bf0c0c1031f3
WHF <905372311@qq.com>
2019-05-04 01:06:58 +0800

    侧滑更改头像&上传数据库

---
+++ ResideMenu - 0504/app/src/main/java/com/example/residemenu/Menu_Setting.java
@@ -0,0 +1,489 @@
+package com.example.residemenu;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import java.io.File;
+
+import cn.bmob.v3.datatype.BmobFile;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.UpdateListener;
+import cn.bmob.v3.listener.UploadFileListener;
+
+public class Menu_Setting extends AppCompatActivity {
+
+    private Button back;
+    private ImageView mImage;
+    private TextView username;
+    private TextView signature;
+    private TextView sexuality;
+    private TextView age;
+    private TextView location;
+    private TextView favorite;
+    private RelativeLayout account;
+
+    private SharedPreferences pref;
+    private String name;
+    private String sign;
+    private String sex;
+    private String year;
+    private String city;
+    private String hobby;
+
+    public static String obgID;
+    private SharedPreferences.Editor editor;
+
+    private Bitmap mBitmap;
+
+    String path = "";
+    private File mFile;
+    protected static final int CHOOSE_PICTURE = 0;
+    protected static final int TAKE_PICTURE = 1;
+    protected static Uri tempUri;
+    private static final int CROP_SMALL_PICTURE = 2;
+
+    public static final int CHOOSE_PHOTO = 2;
+
+    public static final int CUT_PHOTO = 3;
+
+    public static final int TAKE_PHOTO = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu__setting);
+
+        user = (MyUser) getBundle().getSerializable("u");
+
+        mImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String title = "选择获取图片方式";
+                String[] items = new String[]{"拍照", "相册"};
+
+                new AlertDialog.Builder(Menu_Setting.this)
+                        .setTitle(title)
+                        .setItems(items, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                switch (which) {
+                                    case 0:
+                                        //选择拍照
+                                        pickImageFromCamera();
+                                        break;
+                                    case 1:
+                                        //选择相册
+                                        pickImageFromAlbum();
+                                        break;
+                                    default:
+                                        break;
+                                }
+                            }
+                        }).show();
+            }
+        });
+
+        init();
+        //initListeners();
+
+        //按下确认键
+        backListener();
+
+        //按下账户设置键
+        accountListener();
+
+    }
+
+    private void accountListener() {
+        //对"账户设置"按钮的响应，跳转至修改账户状态页面
+        account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!name.equals("")){
+                    Intent intent = new Intent(Menu_Setting.this, Regulate.class);
+                    startActivity(intent);
+                }
+                else{
+                    Intent intent = new Intent(Menu_Setting.this, LoginActivity.class);
+                    startActivity(intent);
+                }
+
+            }
+        });
+    }
+
+    private void init() {
+        pref = getSharedPreferences("LoginData", MODE_PRIVATE);
+        mImage = (ImageView) findViewById(R.id.iv_image);
+        username = (TextView) findViewById(R.id.in_name);
+        signature = (TextView) findViewById(R.id.sign_in);
+        sexuality = (TextView) findViewById(R.id.in_sex);
+        age = (TextView) findViewById(R.id.in_age);
+        location = (TextView) findViewById(R.id.in_city);
+        favorite = (TextView) findViewById(R.id.in_hobby);
+
+        name = pref.getString("userName", "");
+        sign = pref.getString("signature", "");
+        sex = pref.getString("sexuality", "");
+        year = pref.getString("age", "");
+        city = pref.getString("loction", "");
+        hobby = pref.getString("favorite", "");
+
+        if (!name.equals("")) {
+            username.setText(name);
+            signature.setText(sign);
+            sexuality.setText(sex);
+            age.setText(year);
+            location.setText(city);
+            favorite.setText(hobby);
+        } else {
+            username.setText("未登录");
+            signature.setText("未登录");
+            sexuality.setText("");
+            age.setText("");
+            location.setText("");
+            favorite.setText("");
+        }
+    }
+
+        //拍照
+        public void pickImageFromCamera(){
+            String state = Environment.getExternalStorageState();
+            if (state.equals(Environment.MEDIA_MOUNTED)) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File file = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+                if (!file.exists()) {
+                    file.mkdirs();
+                }
+                mFile = new File(file, System.currentTimeMillis() + ".jpg");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mFile));
+                intent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 1);
+                startActivityForResult(intent, TAKE_PHOTO);
+            } else {
+                Toast.makeText(this, "请确认已经插入SD卡", Toast.LENGTH_SHORT).show();
+            }
+        }
+        //从相册获取图片
+        public void pickImageFromAlbum(){
+            Intent picIntent = new Intent(Intent.ACTION_PICK, null);
+            picIntent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+            startActivityForResult(picIntent, CHOOSE_PHOTO);
+        }
+
+        @Override
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            if (resultCode == RESULT_OK) {
+                switch (requestCode) {
+                    case TAKE_PHOTO:
+                        startPhotoZoom(Uri.fromFile(mFile));
+                        break;
+                    case CHOOSE_PHOTO:
+
+                        if (data == null || data.getData() == null) {
+                            return;
+                        }
+                        try {
+                            Bitmap bm = null;
+                            Uri originalUri = data.getData();        //获得图片的uri
+
+
+                            bm = MediaStore.Images.Media.getBitmap(getContentResolver(), originalUri);        //显得到bitmap图片
+
+
+                            //这里开始的第二部分，获取图片的路径：
+
+
+                            String[] proj = {MediaStore.Images.Media.DATA};
+
+
+                            //好像是android多媒体数据库的封装接口，具体的看Android文档
+
+                            Cursor cursor = managedQuery(originalUri, proj, null, null, null);
+
+                            //按我个人理解 这个是获得用户选择的图片的索引值
+
+                            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+
+                            //将光标移至开头 ，这个很重要，不小心很容易引起越界
+
+                            cursor.moveToFirst();
+
+                            //最后根据索引值获取图片路径
+
+                            path = cursor.getString(column_index);
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+
+                        startPhotoZoom(data.getData());
+
+                        break;
+                    case CUT_PHOTO:
+
+                        if (data != null) {
+                            setPicToView(data);
+                        }
+                        break;
+
+
+                }
+            }
+
+        }
+        /**
+         * 打开系统图片裁剪功能
+         *
+         * @param uri  uri
+         */
+        private void startPhotoZoom(Uri uri) {
+            Intent intent = new Intent("com.android.camera.action.CROP");
+            intent.setDataAndType(uri, "image/*");
+            intent.putExtra("crop", true);
+            intent.putExtra("aspectX", 1);
+            intent.putExtra("aspectY", 1);
+            intent.putExtra("outputX", 300);
+            intent.putExtra("outputY", 300);
+            intent.putExtra("scale", true); //黑边
+            intent.putExtra("scaleUpIfNeeded", true); //黑边
+            intent.putExtra("return-data", true);
+            intent.putExtra("noFaceDetection", true);
+            startActivityForResult(intent, CUT_PHOTO);
+
+        }
+        private void setPicToView(Intent data) {
+            Bundle bundle = data.getExtras();
+            if (bundle != null) {
+
+//
+//            Uri selectedImage = data.getData();
+//
+//            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+//
+//            Cursor cursor = getContentResolver().query(selectedImage,
+//                    filePathColumn, null, null, null);
+//            cursor.moveToFirst();
+//
+//            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+//            String picturePath = cursor.getString(columnIndex);
+//
+
+                //这里也可以做文件上传
+                mBitmap = bundle.getParcelable("data");
+                // ivHead.setImageBitmap(mBitmap);
+                mImage.setImageBitmap(mBitmap);
+//
+//            if (picturePath!=null){
+//                path = picturePath;
+//            }
+
+                if(mFile != null){
+                    path = mFile.getPath();
+                }
+
+                Toast.makeText(Menu_Setting.this,"path:"+path,Toast.LENGTH_SHORT).show();
+
+                final BmobFile bmobFile = new BmobFile(new File(path));
+                //Bmob这个上传文件的貌似不成功..........................
+                bmobFile.uploadblock(new UploadFileListener() {
+
+                    @Override
+                    public void done(BmobException e) {
+                        if (e == null) {
+                            Toast.makeText(Menu_Setting.this, "pic is success", Toast.LENGTH_SHORT).show();
+                            // MyUser myUser =MyUser.getCurrentUser(MyUser.class);
+                            //得到上传的图片地址
+                            String fileUrl = bmobFile.getFileUrl();
+                            user.setAvatar(fileUrl);
+                            //更新图片地址
+                            user.update(user.getObjectId(), new UpdateListener() {
+                                @Override
+                                public void done(BmobException e) {
+                                    if (e == null) {
+                                        Toast.makeText(Menu_Setting.this, "update", Toast.LENGTH_SHORT).show();
+
+                                    }
+                                }
+                            });
+                        }
+                    }
+                });
+
+            }
+        }
+
+       // setImageToView(getIntent());
+
+
+    //private void initListeners() {
+        //mImage.setOnClickListener(new View.OnClickListener() {
+           // @Override
+           // public void onClick(View v) {
+
+           // }
+      //  });
+    //        }
+
+
+
+    /**
+     * 显示修改图片的对话框
+     */
+
+    /****
+    protected void showChoosePicDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(Menu_Setting.this);
+
+        builder.setTitle("添加图片");
+        String[] items = { "选择本地照片", "拍照" };
+
+        builder.setNegativeButton("取消", null);
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case CHOOSE_PICTURE: // 选择本地照片
+                        Intent openAlbumIntent = new Intent (Intent.ACTION_GET_CONTENT);
+
+                        openAlbumIntent.setType("image/*");
+                        //用startActivityForResult方法，待会儿重写onActivityResult()方法，拿到图片做裁剪操作
+                        startActivityForResult(openAlbumIntent, CHOOSE_PICTURE);
+                        break;
+                    case TAKE_PICTURE: // 拍照
+                        Intent openCameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        tempUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(), "temp_image.jpg"));
+
+                        // 将拍照所得的相片保存到SD卡根目录
+                        openCameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, tempUri);
+                        startActivityForResult(openCameraIntent, TAKE_PICTURE);
+                        break;
+                }
+            }
+
+        });
+        builder.show();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == MainActivity.RESULT_OK) {
+            switch (requestCode) {
+                case TAKE_PICTURE:
+                    cutImage(tempUri); // 对图片进行裁剪处理
+                    break;
+                case CHOOSE_PICTURE:
+                    cutImage(data.getData()); // 对图片进行裁剪处理
+                    break;
+                case CROP_SMALL_PICTURE:
+                    if (data != null) {
+                        setImageToView(data); // 让刚才选择裁剪得到的图片显示在界面上
+                    }
+                    break;
+            }
+        }
+    }
+
+
+    protected void cutImage(Uri uri) {
+        if (uri == null) {
+            Log.i("alanjet", "The uri is not exist.");
+        }
+        tempUri = uri;
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        //com.android.camera.action.CROP这个action是用来裁剪图片用的
+        intent.setDataAndType(uri, "image/*");
+        // 设置裁剪
+        intent.putExtra("crop", "true");
+        // aspectX aspectY 是宽高的比例
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        // outputX outputY 是裁剪图片宽高
+        intent.putExtra("outputX", 150);
+        intent.putExtra("outputY", 150);
+        intent.putExtra("return-data", true);
+        startActivityForResult(intent, CROP_SMALL_PICTURE);
+    }
+
+
+    protected void setImageToView(Intent data) {
+        Bundle extras = data.getExtras();
+        if (extras != null) {
+            mBitmap = extras.getParcelable("data");
+            mImage.setImageBitmap(mBitmap);//显示图片
+        }
+    }
+    ****/
+
+    //返回键监听
+    private void backListener(){
+        back = (Button)findViewById(R.id.title_back);
+        back.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                name = username.getText().toString();
+                sign = signature.getText().toString();
+                sex = sexuality.getText().toString();
+                year = age.getText().toString();
+                city = location.getText().toString();
+                hobby = favorite.getText().toString();
+
+                final MyUser infor = new MyUser();
+                infor.setUsername(name);
+                infor.setSignature(sign);
+                infor.setSex(sex);
+                infor.setAge(year);
+                infor.setLocation(city);
+                infor.setFavorite(hobby);
+
+                infor.update(new UpdateListener<MyUser>() {
+                    @Override
+                    public void done(BmobException e) {
+                        if (e == null) {
+                            Toast.makeText(Menu_Setting.this, "修改成功咯", Toast.LENGTH_SHORT).show();
+                            obgID = infor.getObjectId();
+                        } else {
+                            Toast.makeText(Menu_Setting.this, "修改失败咯", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+                editor.putString("userName", name);
+                editor.putString("signiture", sign);
+                editor.putString("sexuality", sex);
+                editor.putString("age", year);
+                editor.putString("location", city);
+                editor.putString("favorita", hobby);
+                editor.putBoolean("isLogin", true);
+                editor.apply();
+
+
+                //返回
+                finish();
+            }
+        });
+    }
+
+
+
+}
---
+++ ResideMenu - 0504/app/src/main/java/com/example/residemenu/Regulate.java
@@ -0,0 +1,108 @@
+package com.example.residemenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.EditText;
+import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.Toast;
+import android.util.Log;
+
+import cn.bmob.v3.Bmob;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.UpdateListener;
+
+
+
+public class Regulate extends AppCompatActivity {
+
+    private TextView quit;
+    private TextView delete;
+    private Button back;
+
+
+    private String phone;
+
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regulate);
+
+        init();
+        initListener();
+        //初始化Bmob
+        Bmob.initialize(this, "1768baa1a8670ebb020369f5672e443f" );
+    }
+
+    private void initListener() {
+        //对返回按钮的响应，跳转至个人中心页面
+        back.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                finish();
+            }
+        });
+
+
+        //对"退出登录"的响应，跳转至登录页面
+        quit.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                editor.putBoolean("isLogin", false);
+                editor.apply();
+
+                Intent intent = new Intent(Regulate.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        //对"删除账户"的响应，跳转至登录页面
+        delete.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                MyUser us = new MyUser();
+                us.setObjectId(LoginActivity.obgID);
+                us.delete(new UpdateListener() {
+
+                    @Override
+                    public void done(BmobException e) {
+                        if(e==null){
+                            Log.i("bmob","成功");
+                        }else{
+                            Log.i("bmob","失败："+e.getMessage()+","+e.getErrorCode());
+                        }
+                    }
+                });
+                editor.clear();
+                editor.putBoolean("isLogin", false);
+                editor.apply();
+
+                Intent intent = new Intent(Regulate.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private  void init(){
+        pref = getSharedPreferences("LoginData", MODE_PRIVATE);
+        editor = pref.edit();
+        editor.apply();
+
+        back = (Button) findViewById(R.id.regulate_title_back);
+        quit = (TextView) findViewById(R.id.txt_quit);
+        delete = (TextView) findViewById(R.id.txt_delete);
+
+    }
+
+
+}
